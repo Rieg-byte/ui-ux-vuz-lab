36299285f49760bc7c409995421fad982825918c
Lineked <lineked@yandex.ru>
2024-10-10 18:35:38 +0300

    add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './header.module.css';
+export const Header = () => {
+    const navigate = useNavigate();
+    return (
+        <header className={styles.header}>
+            <nav>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}>
+                        <button onClick={() => navigate('/')}>Home</button>
+                    </li>
+                    <li className={styles.navItem}>
+                        <button onClick={() => navigate('/about')}>About
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
